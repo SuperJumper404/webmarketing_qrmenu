3dabc0e8f5ff7a43c6433e5350cc14d8cec95195
joker <ns.ayness@gmail.com>
2023-06-17 19:41:30 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    ssr: false,
+    modules: [
+      '@nuxtjs/tailwindcss'
+    ],
+    tailwindcss: {
+      cssPath: '~/assets/css/tailwind.css',
+      configPath: 'tailwind.config',
+      exposeConfig: false,
+      exposeLevel: 2,
+      injectPosition: 'first',
+      viewer: true,
+    }
+  })
